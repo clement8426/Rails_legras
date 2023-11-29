d80ae3d80049bb62e8eb6439cd4e73755a6984e0
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2023-11-29 14:08:08 +0100

    add category and start searchbar

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,4 +1,11 @@
 class Network < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_location_and_description,
+  against: [ :location, :description ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
