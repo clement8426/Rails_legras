f8ae626a623df5a76bf911ea3e12e064bda565ab
emmanuel etienne <emn.etienne@gmail.com>
2023-11-29 14:08:38 +0100

    ingredient seed

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,18 @@
 class Ingredient < ApplicationRecord
-  has_many :recipes_ingredients
-  has_many :recipes, through: :recipes_ingredients
+  VALID_CATEGORIES = [
+    "Viandes et volailles",
+    "Produits laitiers",
+    "Fromages",
+    "fruits",
+    "légumes",
+    "Céréales",
+    "Vin et spiritueux",
+    "Boissons non alcoolisées",
+    "Epices et aromates"
+  ].freeze
+
+  validates :category, inclusion: { in: VALID_CATEGORIES, message: "%<value> n'est pas une catégorie valide" }
 end
+has_many :recipes_ingredients
+has_many :recipes, through: :recipes_ingredients
+validates :category, presence: true
