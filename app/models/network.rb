4904ed823df6de541b4e08993e3493582a5e03b8
emmanuel etienne <emn.etienne@gmail.com>
2023-11-30 16:21:35 +0100

    qlkfqksds

--- app/models/network.rb
+++ app/models/network.rb
@@ -2,6 +2,8 @@ class Network < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
+  has_many_attached :photo
+
   include PgSearch::Model
   pg_search_scope :search_by_location_and_description,
   against: [ :name, :location, :description ],
