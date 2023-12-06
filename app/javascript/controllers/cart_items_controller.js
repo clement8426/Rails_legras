6ebeb688e1eed24a477c5fc65f8c6d31cae5d8ba
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2023-12-06 15:24:19 +0100

    ok

--- app/javascript/controllers/cart_items_controller.js
+++ app/javascript/controllers/cart_items_controller.js
@@ -3,13 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="cart-items"
 export default class extends Controller {
   static targets = ["form", "list", "cart", "trash"]
-
+  static values = { id: Number }
   connect() {
   }
 
   create(event) {
     event.preventDefault();
-    // console.log(event.currentTarget)
     this.form = this.formTargets.find((form) => form === event.currentTarget)
     const url = this.form.action
     console.log(url);
@@ -20,10 +19,25 @@ export default class extends Controller {
     })
       .then(response => response.json())
       .then((data) => {
-        // console.log(data.html)
         this.cartTarget.innerHTML = data.html
-        // console.log(this.formTargets)
-        // this.formTargets.forEach((form) => form.reset() )
       })
+  }
+
+  delete(event) {
+    event.preventDefault();
+    this.trash = this.trashTargets.find((trash) => trash === event.currentTarget)
+    this.cartItemId = parseInt(this.trash.dataset.cartItemsIdValue)
+    this.cartItemId
+    const url = this.trash.href
+    fetch(url, {
+      method: "DELETE",
+      headers: { "Accept": "application/json" },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        // this.cartTarget.innerHTML = data.html
+      })
+
   }
 }
