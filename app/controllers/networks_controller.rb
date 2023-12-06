b2fc57cb93f3f2c8c379d2ba851262aa5ef1e34e
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2023-12-06 14:06:12 +0100

    push add to cart

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -20,7 +20,10 @@ class NetworksController < ApplicationController
     @cart_item = CartItem.new
     @network = Network.find(params[:id])
     @network_farmers = User.where(type: 'farmer', network_id: @network.id)
-
-    @cart = Cart.find_by(network_id: @network.id, user_id: current_user.id)
+    if Cart.find_by(network_id: @network.id, user_id: current_user.id).nil?
+      @cart = Cart.create(network_id: @network.id, user_id: current_user.id)
+    else
+      @cart = Cart.find_by(network_id: @network.id, user_id: current_user.id)
+    end
   end
 end
