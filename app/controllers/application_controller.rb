9ed9fa0f131f0ae87f6ba9eb06e39b8af132219b
le du anne-charlotte <anne-charlotte.ld@live.fr>
2023-12-07 12:33:25 +0100

    frontancha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  after_action :trigger_job
 
   def trigger_job
     @ingredients = current_user.cart_items.map(&:ingredient)
